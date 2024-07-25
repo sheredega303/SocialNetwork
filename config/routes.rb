f1afda59554dce6256cecac19119bdd59c1703bf
Andrey Sheredega <sheredega303@gmail.com>
2024-07-25 12:23:21 +0300

    Add username to User model, config devise controller and views for that

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registration: 'users/registration'
+  }
   root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
