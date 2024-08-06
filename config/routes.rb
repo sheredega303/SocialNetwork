9e74f6aa3ecf8b5a9af2347ff5c121cae1bc65e5
Andrey Sheredega <sheredega303@gmail.com>
2024-08-06 18:15:44 +0300

    Add show path to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  resources :users, only: [:index, :show]
+
   devise_for :users, controllers: {
     registration: 'users/registrations'
   }
-  root 'pages#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
